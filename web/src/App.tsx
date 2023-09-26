bc73c6bd9789239ae7ae37052c658114bf38a2f4
Simon <simon.riembault@gmail.com>
2023-09-26 20:52:43 +0200

    [AREA-35] feat(web-app): Basic components of welcome page (background and main div)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,21 +4,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App  overflow-hidden w-full h-full flex items-center justify-center">
+      <img src="/assets/vectorBackground.jpg" alt="background" className=" w-full h-screen object-cover z-0"/>
+      <div className='bg-white rounded-3xl absolute h-3/5 w-4/12 z-10'>
+        <h1 className='text-4xl font-bold my-10'>Welcome to App Name</h1>
+        
+      </div>
     </div>
   );
 }
