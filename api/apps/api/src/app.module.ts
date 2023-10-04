f279c9da9a5a50b2b4a0d7ecd64ec25e9a885c1c
Nather_ / Nathan Delenclos <nathan.delenclos@epitech.eu>
2023-10-04 16:36:22 +0200

    feat: add host and port of mysql in env

--- api/apps/api/src/app.module.ts
+++ api/apps/api/src/app.module.ts
@@ -39,8 +39,8 @@ import { ServiceController } from './controllers/service.controller';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'mysql',
-        host: 'localhost',
-        port: 3306,
+        host: configService.get('MYSQL_HOST'),
+        port: configService.get('MYSQL_PORT'),
         username: configService.get('MYSQL_USER'),
         password: configService.get('MYSQL_PASSWORD'),
         database: configService.get('MYSQL_DATABASE'),
--- api/apps/discord/src/discord.module.ts
+++ api/apps/discord/src/discord.module.ts
@@ -21,8 +21,8 @@ import { ReactionEntity } from '@app/common/reactions/reaction.entity';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'mysql',
-        host: 'localhost',
-        port: 3306,
+        host: configService.get('MYSQL_HOST'),
+        port: configService.get('MYSQL_PORT'),
         username: configService.get('MYSQL_USER'),
         password: configService.get('MYSQL_PASSWORD'),
         database: configService.get('MYSQL_DATABASE'),
