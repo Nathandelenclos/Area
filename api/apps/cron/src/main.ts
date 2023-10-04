2d89686ff66eb80eabac7f30e6c1c894af5561e6
Nather_ / Nathan Delenclos <nathan.delenclos@epitech.eu>
2023-10-04 16:40:00 +0200

    feat: use MicroServiceInit for connect cron & auth microservice

--- api/apps/cron/src/main.ts
+++ api/apps/cron/src/main.ts
@@ -3,28 +3,17 @@ import { ConfigService } from '@nestjs/config';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { CronModule } from './cron.module';
 import MicroServiceProxy from '@app/common/micro.service.proxy';
+import MicroServiceInit from '@app/common/micro.service.init';
 
 async function bootstrap() {
   const app = await NestFactory.create(CronModule);
-
-  // Get the environment variables from the ConfigService
   const configService = app.get(ConfigService);
 
-  const USER = configService.get('RABBITMQ_USER');
-  const PASSWORD = configService.get('RABBITMQ_PASS');
-  const HOST = configService.get('RABBITMQ_HOST');
-
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.RMQ,
-    options: {
-      urls: [`amqp://${USER}:${PASSWORD}@${HOST}`],
-      noAck: false,
-      queue: MicroServiceProxy.microServiceQueue.CRON_SERVICE,
-      queueOptions: {
-        durable: true,
-      },
-    },
-  });
+  MicroServiceInit.connect(
+    app,
+    configService,
+    MicroServiceProxy.microServiceQueue.CRON_SERVICE,
+  );
 
   app.startAllMicroservices();
 }
