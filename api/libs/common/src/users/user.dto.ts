1a80e3efd4221e89354e373104d6eabaced97ff7
Clément <clement.vandeville@epitech.eu>
2023-10-09 11:22:48 +0200

    fix: Provider add to user dto

--- api/libs/common/src/users/user.dto.ts
+++ api/libs/common/src/users/user.dto.ts
@@ -4,9 +4,11 @@ export interface NewUserDto {
   name: string;
 }
 
+export type Providers = 'google' | 'facebook' | 'github';
+
 export interface NewUserOAuthDto {
   email: string;
-  provider: 'Google' | '';
+  provider: Providers;
   token: string;
   name: string;
 }
@@ -18,6 +20,6 @@ export interface UserCredentialsDto {
 
 export interface UserOAuthCredentialsDto {
   email: string;
-  provider: 'Google' | '';
+  provider: Providers;
   token: string;
 }
