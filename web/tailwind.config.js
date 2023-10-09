a6429a955419519dca802268d5049d4b462455e6
Nather_ / Nathan Delenclos <nathan.delenclos@epitech.eu>
2023-10-09 15:43:42 +0200

    feat: responsive auth pages

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,tsx}"],
   theme: {
+    screens: {
+      'sm': '576px',
+      'md': '960px',
+      'lg': '1440px',
+    },
     extend: {},
   },
   plugins: [],
