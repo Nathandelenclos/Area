a7b50900228a5d85981dfc65df998a11a35160fe
Nather_ / Nathan Delenclos <nathan.delenclos@epitech.eu>
2023-10-09 14:38:46 +0200

    feat: login request

--- web/src/services/AuthServices.ts
+++ web/src/services/AuthServices.ts
@@ -1,11 +1,49 @@
 import { ApiInvoke } from "@src/services/api/api.invoke";
 
 export class AuthServices {
+  /**
+   * Register a new user
+   * @param name
+   * @param email
+   * @param password
+   */
   static register(name: string, email: string, password: string) {
     return ApiInvoke({
       endpoint: `/auth/register`,
       method: "POST",
       body: JSON.stringify({ name, email, password }),
+      expectedStatus: 200,
+    });
+  }
+
+  static registerOAuth(
+    name: string,
+    email: string,
+    provider: string,
+    token: string,
+  ) {
+    return ApiInvoke({
+      endpoint: `/auth/register-oauth`,
+      method: "POST",
+      body: JSON.stringify({ name, email, provider, token }),
+      expectedStatus: 200,
+    });
+  }
+
+  static login(email: string, password: string) {
+    return ApiInvoke({
+      endpoint: `/auth/signin`,
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      expectedStatus: 200,
+    });
+  }
+
+  static loginOAuth(email: string, provider: string, token: string) {
+    return ApiInvoke({
+      endpoint: `/auth/signin-oauth`,
+      method: "POST",
+      body: JSON.stringify({ email, provider, token }),
       expectedStatus: 200,
     });
   }
