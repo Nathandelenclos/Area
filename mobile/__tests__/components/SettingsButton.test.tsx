07c5f2cf439499e683cdc94513aa7f52971b2288
David Plouvier <david.plouvier@epitech.eu>
2023-11-04 18:28:11 +0100

    test: fix test settingsButton

--- mobile/__tests__/components/SettingsButton.test.tsx
+++ mobile/__tests__/components/SettingsButton.test.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 import { it, describe, expect, jest } from '@jest/globals';
 import { render, fireEvent } from '@testing-library/react-native';
 import SettingsButton from '@components/SettingsButton';
+import { View } from 'react-native';
 
-jest.mock('@fortawesome/react-fontawesome', () => ({
-  FontAwesomeIcon: () => <i className="fa fa-circle"></i>,
+jest.mock("@fortawesome/react-native-fontawesome", () => ({
+  FontAwesomeIcon: () => <div>Icon</div>,
 }));
 
 describe('SettingsButton component', () => {
     it('renders correctly with given buttonColor and onPress', () => {
       const onPressMock = jest.fn();
-      const { getByText, getByTestId } = render(
-        <SettingsButton buttonColor="Test Title" onPress={onPressMock} />
+      const { getByTestId } = render(
+        <SettingsButton buttonColor="red" onPress={onPressMock} />
       );
       const button = getByTestId('settings-button');
-      expect(button).toBeTruthy();
-      expect(getByText('Test Title')).toBeTruthy();
+      expect(getByTestId('settings-button')).toBeTruthy();
     
       // Simuler un appui sur le bouton et vérifier si la fonction onPress est appelée.
       fireEvent.press(button);
